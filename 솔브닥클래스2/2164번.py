75036848a168ada37c4bb4a99b6732d9dfcb2d80
ChoiPython <wndud7159@gmail.com>
2022-10-20 14:29:26 +0900

    솔브닥클래스2 - 2164번 - "카드2" - 단순히 리스트로 구현하는 것 보다 deque 라이브러리를 불러와서 하는게 더 빠르다.

--- 솔브닥클래스2/2164번.py
+++ 솔브닥클래스2/2164번.py
@@ -5,7 +5,22 @@
 N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오.
 
 '''
+#
+'''
+https://pacific-ocean.tistory.com/61 - 규칙성을 발견하고 n에 대하여 그냥 바로 출력
+a = int(input())
+b = 2
+while True:
+    if a == 1 or a == 2:
+        print(a)
+        break
+    b *= 2
+    if b >= a:
+        print((a - (b // 2)) * 2)
+        break
+'''
 
+''' 내가 만든 덱
 def deque(num, stop) :
     if stop:
         num.pop(0)
@@ -43,4 +58,38 @@ while True:
 
 print(num[0])
 
+'''
 
+
+''' 덱 라이브러리 불러옴
+from collections import deque
+
+d = deque()
+n = int(input())
+
+# 덱에 추가
+for i in range(1, n + 1) :
+    d.append(i)     # appnedleft - 왼쪽 부터 삽입
+                    # extend(left)[1, 2, 3] - 한번에 추가
+
+while len(d) != 1 :
+    d.popleft()
+    d.append(d.popleft())
+
+print(d[0])
+'''
+
+'''?? 리스트랑 뭐가 다르지
+
+n= int(input())
+num = []
+
+for i in range(1, n + 1):
+    num.append(i)
+
+while len(num) != 1:
+    num.pop(0)
+    num.append(num.pop(0))
+
+print(num[0])
+'''
