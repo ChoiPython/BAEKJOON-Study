34e379af455e21140cac544f156ac7909bc8fd4a
ChoiPython <wndud7159@gmail.com>
2023-03-08 21:21:00 +0900

    솔브닥클래스2 - 4949번 - '균형잡힌 세상'

--- 솔브닥클래스2/4949번.py
+++ 솔브닥클래스2/4949번.py
@@ -7,59 +7,107 @@
 모든 괄호들의 짝은 1:1 매칭만 가능하다. 즉, 괄호 하나가 둘 이상의 괄호와 짝지어지지 않는다.
 짝을 이루는 두 괄호가 있을 때, 그 사이에 있는 문자열도 균형이 잡혀야 한다.
 '''
-stack = []
+
+# 
+# stack = []
+# string = ''
+# while True:
+#     string = input()
+#     box = []
+
+#     if string == '.' :
+#         break
+    
+#     check = 0
+
+#     # 열기, 닫기 개수는 맞지만 매칭되는 괄호 오류는 해결x
+#     for i in range(len(string)) :
+#         # 대괄호 추가
+#         if string[i] == '[' :
+#             box.append('[')
+#             check += 1
+        
+#         # 대괄호 닫기
+#         elif string[i] == ']' :
+#             try :
+
+#                 if box[check - 1] == '[' :
+#                     check -= 1
+#                     box.pop(-1)
+                
+#                 else :
+#                     box.append('false') 
+                    
+#             except :
+#                 box.append('false')
+#                 break
+
+#         # 소괄호 추가
+#         if string[i] == '(' :
+#             box.append('(')
+#             check += 1
+
+#         # 소괄호 닫기
+#         elif string[i] == ')' :
+#             try :
+#                 if box[check - 1] == '(' :
+#                     check -= 1
+#                     box.pop(-1)
+                    
+#                 else:
+#                     box.append('false')
+
+#             except :
+#                 box.append('false')
+    
+#     if len(box) == 0 :
+#         print('yes')
+
+#     else :
+#         print('no')
+
+
 string = ''
-while True:
+
+while True :
     string = input()
-    box = []
+    stack = []
+
+    check = -1       # 0 = 소괄호 / 1 = 대괄호
 
     if string == '.' :
         break
-    
-    check = 0
 
-    # 열기, 닫기 개수는 맞지만 매칭되는 괄호 오류는 해결x
-    for i in range(len(string)) :
-        # 대괄호 추가
-        if string[i] == '[' :
-            box.append('[')
-            check += 1
-        
-        # 대괄호 닫기
-        elif string[i] == ']' :
+    count = 0
+
+    for i in string :
+        if i in ['(', ')', '[', ']'] :
+
+            if i == '(' or i == '[' :
+                stack.append(i)
+
             try :
-
-                if box[check - 1] == '[' :
-                    check -= 1
-                    box.pop(-1)
-                
-                else :
-                    box.append('false') 
+                if i == ')' :
+                    if stack[-1] == '(' :
+                        stack.pop(-1)
                     
-            except :
-                box.append('false')
-                break
+                    else:
+                        stack.append('false')
 
-        # 소괄호 추가
-        if string[i] == '(' :
-            box.append('(')
-            check += 1
-
-        # 소괄호 닫기
-        elif string[i] == ')' :
-            try :
-                if box[check - 1] == '(' :
-                    check -= 1
-                    box.pop(-1)
+                elif i == ']' :
+                    if stack[-1] == '[' : 
+                        stack.pop(-1)
                     
-                else:
-                    box.append('false')
-
+                    else:
+                        stack.append('false')
             except :
-                box.append('false')
-    
-    if len(box) == 0 :
+                stack.append('false')
+
+    if len(stack) == 0 :
         print('yes')
 
     else :
         print('no')
+
+
+
